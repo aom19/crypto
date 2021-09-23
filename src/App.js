1d9db8e9864ab9bf48e499d834de56103ade8126
aom19 <39848284+aom19@users.noreply.github.com>
2021-09-23 13:16:27 +0300

    Set the routes and create Navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import React from "react";
+import "./App.css";
 
-import './App.css';
+import { Switch, Route, Link } from "react-router-dom";
+import { Layout, Typography, Space } from "antd";
+
+//Components
+import { Navbar } from "./components";
 
 function App() {
   return (
     <div className="App">
-     <h1> Heelo</h1>
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="main"></div>
+      <div className="footers"></div>
     </div>
   );
 }
